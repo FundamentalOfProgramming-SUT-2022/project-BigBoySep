fbfcd9c685b7a8ac6700a459503c6382a60f76dd
Sepehr <38585825+BigBoySep@users.noreply.github.com>
2023-01-14 14:27:43 +0330

    insertstr added

--- main.c
+++ main.c
@@ -9,12 +9,8 @@
 int line,pos;
 char command[50],dir[100],tmp[100],str[1000],buff_str[1000];
 
-void empty(char str[]){
-    for(int i =0;i<strlen(str);i++)
-        str[i] = '\0';
-}
 
-void create_file(){
+void createfile(){
     if (access(dir, F_OK) == 0) {
         // file exists
         printf("File exists\n");
@@ -32,8 +28,49 @@ void create_file(){
     }
     FILE *fp;
     fp = fopen(tmp,"w");
-    empty(tmp);
+    memset(tmp,0,sizeof(tmp));
     fclose(fp);
+}
+//insertstr
+void insertstr(){
+    FILE *fp,*tmpf;
+    fp = fopen(dir,"r");
+    strncpy(tmp,dir,strlen(dir)-4);
+    strcat(tmp,"____temp.txt"); 
+    tmpf = fopen(tmp,"w+");
+    // fputs(str,tmpf);
+    bool keep_reading = true;
+    int current_line = 1;
+    while (keep_reading){
+        fgets(buff_str,1000,fp);
+        if(feof(fp)) keep_reading = false;
+        // printf("%d",keep_reading);
+        if(line == current_line){
+            for(int i =0;i<strlen(buff_str);i++){
+                if(i==pos)
+                    fputs(str,tmpf);
+                fputc(buff_str[i],tmpf);
+            }
+            if(strlen(buff_str) == pos)
+                fputs(str,tmpf);
+            // fprintf(tmpf,"chert\nchert");
+            // printf("aa\n");
+            // printf("%s",str);
+            // printf("%s",buff_str);
+        }else{
+        fputs(buff_str,tmpf);
+        }
+        current_line++;
+    }
+    if(current_line == line){
+        fputc('\n',tmpf);
+        fputs(str,tmpf);
+    }
+    fclose(tmpf);
+    fclose(fp);
+    remove(dir);
+    rename(tmp,dir);
+    memset(tmp,0,sizeof(tmp));
 }
 
 void input(){
@@ -53,7 +90,7 @@ void input(){
             dir[t] = '\0';
         }
         // printf("%s\n",dir);
-        create_file();
+        createfile();
         return;
     }else if(strcmp(command,"insertstr") == 0){
         scanf("%s",command);
@@ -73,6 +110,7 @@ void input(){
         if (access(dir, F_OK) == 0) {
         }else{
             printf("file not found\n");
+            bool not_found = true;
         }
         c = getchar();
         scanf("%s",command);
@@ -80,14 +118,48 @@ void input(){
         c = getchar();
         if(c!='"'){
             str[0] = c;
-            scanf("%s",buff_str);
-            strcat(str,buff_str);
+            int t= 1;
+            while ((c=getchar()) != ' ')
+            {
+                str[t] = c;
+                if(str[t] == 'n' && str[t-1]=='\\' && str[t-2]!='\\'){
+                    str[t-1] = '\n';
+                    str[t] = '\0';
+                    t--;
+                }
+                if(str[t] == 'n' && str[t-1]=='\\' && str[t-2]=='\\'){
+                    str[t-1] = 'n';
+                    str[t] = '\0';
+                    t--;
+                }
+                if(str[t] == '"' && str[t-1] == '\\'){
+                    str[t-1] = '"';
+                    str[t] = '\0';
+                    t--;
+                }
+                t++;
+            }
         }
         else{
             int t =0;
-            c = getchar();
+            // c = getchar();
             while ((c=getchar()) != '"' || str[t-1] == '\\'){
                 str[t]=c;
+                if(str[t] == 'n' && str[t-1]=='\\' && str[t-2]!='\\'){
+                    str[t-1] = '\n';
+                    str[t] = '\0';
+                    t--;
+                }
+                if(str[t] == 'n' && str[t-1]=='\\' && str[t-2]=='\\'){
+                    str[t-1] = 'n';
+                    str[t] = '\0';
+                    t--;
+                }
+                if(str[t] == '"' && str[t-1] == '\\'){
+                    str[t-1] = '"';
+                    str[t] = '\0';
+                    t--;
+                }
                 t++;
             }
             str[t] = '\0';
@@ -96,6 +168,9 @@ void input(){
         scanf("%s",command);
         c = getchar();
         scanf("%d:%d",&line,&pos);
+        // printf("%s",str);
+        insertstr();
+        memset(str,0,sizeof(str));
         return;
         
     }else{
