de5b41877d334a62e6b9c39980d500e29559ec0d
Sepehr <38585825+BigBoySep@users.noreply.github.com>
2023-01-14 14:23:43 +0330

    insertstr added

--- main.c
+++ main.c
@@ -6,10 +6,11 @@
 #include <errno.h>
 #include <string.h>
 
-char command[50],dir[100],tmp[100];
+int line,pos;
+char command[50],dir[100],tmp[100],str[1000],buff_str[1000];
 
 void empty(char str[]){
-    for(int i =0;i<100;i++)
+    for(int i =0;i<strlen(str);i++)
         str[i] = '\0';
 }
 
@@ -54,6 +55,49 @@ void input(){
         // printf("%s\n",dir);
         create_file();
         return;
+    }else if(strcmp(command,"insertstr") == 0){
+        scanf("%s",command);
+        char c = getchar();
+        c = getchar();
+        if(c=='/')
+            scanf("%s",dir);
+        else{
+            int t =0;
+            c = getchar();
+            while ((c=getchar()) != '"'){
+                dir[t]=c;
+                t++;
+            }
+            dir[t] = '\0';
+        }
+        if (access(dir, F_OK) == 0) {
+        }else{
+            printf("file not found\n");
+        }
+        c = getchar();
+        scanf("%s",command);
+        c = getchar();
+        c = getchar();
+        if(c!='"'){
+            str[0] = c;
+            scanf("%s",buff_str);
+            strcat(str,buff_str);
+        }
+        else{
+            int t =0;
+            c = getchar();
+            while ((c=getchar()) != '"' || str[t-1] == '\\'){
+                str[t]=c;
+                t++;
+            }
+            str[t] = '\0';
+        }
+        c = getchar();
+        scanf("%s",command);
+        c = getchar();
+        scanf("%d:%d",&line,&pos);
+        return;
+        
     }else{
         printf("invalid command\n");
         gets(command);
@@ -63,7 +107,6 @@ void input(){
 
 
 int main(){
-
     scanf("%s",command);
     while (strcmp(command,"exit") != 0){
         input();
